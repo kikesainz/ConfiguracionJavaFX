f133e938098ae4efae85852aee51519c321546ce
kikesainz <kikesainz>
2021-05-29 17:03:26 +0200

    [ADD] Controller

--- src/hola/Controller.java
+++ src/hola/Controller.java
@@ -1,4 +1,15 @@
 package hola;
+
+import javafx.scene.control.Button;
+
 public class Controller {
+	
+	Button boton;
+	
+	public void atiendeClickBoton(){
+		
+		System.out.println("Se ha pulsado el botón");
+		System.out.println("Llamaré a la clase correspondiente para hacer el trabajo que corresponda....");
+	}
 
 }
